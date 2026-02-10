db75a91c99d3dcdc2a2dc4259d2f042eb3614fca
Juanmafa2004 <juanmanuelforero2004@outlook.com>
2026-02-10 15:11:08 -0500

    Se completo el testing de los callbacks y de la primera parte de factory

--- Secciones/Seccion2_bases/parte 3 migracion a typescript/tests/js-foundation/03-callbacks.test.ts
+++ Secciones/Seccion2_bases/parte 3 migracion a typescript/tests/js-foundation/03-callbacks.test.ts
@@ -15,6 +15,18 @@ describe('js-foundation/03-callbacks.ts', () => {
         });
     });
 
+    test('getUserById should return John Doe', (done) => {
+        const id = 1;
+        getUserById(id, (err, user) => {
+            expect(err).toBeUndefined();
+            expect(user).toStrictEqual({
+                id: 1,
+                name: 'John Doe',
+            })
+            done();
+        })
+
+    })
 
 
 });
