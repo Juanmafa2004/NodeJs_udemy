a1a271252458ee2deb516520ba5f647b60fdfb09
Juanmafa2004 <juanmanuelforero2004@outlook.com>
2026-02-23 14:47:44 -0500

    Se inicio creando una aplicacion sencilla de consola

--- Secciones/Seccion4_clean_architecture/01-multiplicationApp/src/app.ts
+++ Secciones/Seccion4_clean_architecture/01-multiplicationApp/src/app.ts
@@ -1,5 +1,44 @@
+import fs from 'fs';
 
-const message: string = "Hello World";
+// const message: string = "Hello World";
 
 
-console.log(message);
+// console.log(message);
+
+
+//ejercicio de tarea
+
+// const num: number = 5;
+
+
+// const tablaMultiplicar = (num: number) => {
+
+//     console.log(`Tabla de multiplicar del ${num}`);
+
+//     for (let i = 1; i <= 10; i++) {
+//         console.log(`${num} x ${i} = ${num * i}`);
+//     }
+
+// }
+
+// tablaMultiplicar(num);
+
+
+let outputMessage = '';
+const base = 5;
+const headerMessage = `
+====================================
+  Tabla de multiplicar del ${base}
+====================================
+`;
+
+for (let i = 1; i <= 10; i++) {
+    outputMessage += `${base} x ${i} = ${base * i}\n`;
+}
+outputMessage = headerMessage + outputMessage;
+
+const outPutPath = `outputs`
+
+fs.mkdirSync(outPutPath, { recursive: true });
+
+fs.writeFileSync(`${outPutPath}/tabla-${base}.txt`, outputMessage);
